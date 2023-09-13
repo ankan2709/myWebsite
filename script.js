91ddf2aad7f274deeaf8833e77ac7a54a6d1e4f2
Ankan Dash <ankandash18@gmail.com>
2023-09-12 20:09:16 -0400

    js file

---
+++ script.js
@@ -0,0 +1,37 @@
+// Get references to the section headings
+const educationHeading = document.querySelector("#education h2");
+const experienceHeading = document.querySelector("#experience h2");
+const projectsHeading = document.querySelector("#projects h2");
+const publicationsHeading = document.querySelector("#publications h2");
+
+// Get references to the section contents
+const educationSection = document.querySelector("#education");
+const experienceSection = document.querySelector("#experience");
+const projectsSection = document.querySelector("#projects");
+const publicationsSection = document.querySelector("#publications");
+
+// // Add click event listeners to toggle section visibility
+// educationHeading.addEventListener("click", () => {
+//     toggleVisibility(educationSection);
+// });
+
+// experienceHeading.addEventListener("click", () => {
+//     toggleVisibility(experienceSection);
+// });
+
+// projectsHeading.addEventListener("click", () => {
+//     toggleVisibility(projectsSection);
+// });
+
+// publicationsHeading.addEventListener("click", () => {
+//     toggleVisibility(publicationsSection);
+// });
+
+// // Function to toggle section visibility
+// function toggleVisibility(section) {
+//     if (section.style.display === "block") {
+//         section.style.display = "none";
+//     } else {
+//         section.style.display = "block";
+//     }
+// }
